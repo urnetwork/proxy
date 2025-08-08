fb13106e2933f11c0fb638817243f1af68474f92
Brien Colwell <xcolwell@gmail.com>
2025-08-08 13:06:05 -0500

    http: add http proxy

---
+++ http/dot/dot_resolver_test.go
@@ -0,0 +1,28 @@
+package dot
+
+import (
+	"context"
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDnsCache_Resolve(t *testing.T) {
+	c := dot.NewDNSCache()
+
+	res, err := c.Resolve(context.Background(), "grafana.prod.ur.network")
+	require.NoError(t, err)
+
+	slices.Sort(res)
+	require.Equal(t, []string{"65.49.70.83", "65.49.70.85"}, res)
+
+	res, err = c.Resolve(context.Background(), "grafana.prod.ur.network")
+	require.NoError(t, err)
+
+	slices.Sort(res)
+	require.Equal(t, []string{"65.49.70.83", "65.49.70.85"}, res)
+
+	require.Equal(t, uint64(1), c.LookupsPeformed.Load())
+
+}
