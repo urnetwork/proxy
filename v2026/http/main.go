155ede44649a5e3019890645b8fb3d4a9c4fbefe
Product Builder <builder@bringyour.com>
2026-04-14 20:46:40 -0700

    2026.4.14-914247230

--- v2026/http/main.go
+++ v2026/http/main.go
@@ -21,9 +21,9 @@ import (
 	"github.com/redis/go-redis/v9"
 	"github.com/urfave/cli/v2"
 
-	"github.com/urnetwork/connect"
-	"github.com/urnetwork/proxy/http/connprovider"
-	"github.com/urnetwork/proxy/http/ratelimiter"
+	"github.com/urnetwork/connect/v2026"
+	"github.com/urnetwork/proxy/v2026/http/connprovider"
+	"github.com/urnetwork/proxy/v2026/http/ratelimiter"
 )
 
 
