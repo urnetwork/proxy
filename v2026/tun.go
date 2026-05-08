ecfd3082d12c49849a33fd06c33d9003cb5f3520
Product Builder <builder@bringyour.com>
2026-05-08 15:24:48 -0700

    2026.5.8-934790130

--- v2026/tun.go
+++ v2026/tun.go
@@ -32,8 +32,8 @@ import (
 	"gvisor.dev/gvisor/pkg/tcpip/transport/udp"
 	// "gvisor.dev/gvisor/pkg/waiter"
 
-	"github.com/urnetwork/connect"
-	"github.com/urnetwork/glog"
+	"github.com/urnetwork/connect/v2026"
+	"github.com/urnetwork/glog/v2026"
 )
 
 // const DefaultChannelSize = 64
